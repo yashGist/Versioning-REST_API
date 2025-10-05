f79a30ead10028c6d0e554bb5764f6055bc92e46
Yashdeep <ramawatyashdeep@gmail.com>
2025-10-05 19:57:17 +0530

    code added for URI versioning used by TWITTER

---
+++ src/main/java/com/controller/VersioningPersonController.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+    //URI versioning used by TWITTER
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionOfPerson() {
+        return new PersonV1("Bob Charlie");
+    }
+    //URI versioning used by TWITTER
+    @GetMapping("/v2/person")
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+}
+
--- src/main/java/com/pms/versioningrest_api/VersioningRestApiApplication.java
+++ src/main/java/com/pms/versioningrest_api/VersioningRestApiApplication.java
@@ -3,7 +3,7 @@ package com.pms.versioningrest_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.controller", "com.pms.versioningrest_api"})
 public class VersioningRestApiApplication {
 
     public static void main(String[] args) {
