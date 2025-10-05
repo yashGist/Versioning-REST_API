514afed991dad21bad3907a7fdf41d52ca1b0ac6
Yashdeep <ramawatyashdeep@gmail.com>
2025-10-05 20:49:39 +0530

    URI, Request Parameter, Header, Accept Header/Content Negotiation

--- src/main/java/com/controller/VersioningPersonController.java
+++ src/main/java/com/controller/VersioningPersonController.java
@@ -16,5 +16,42 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
+    //RequestParameter versioning used by AMAZON
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    //RequestParameter versioning used by AMAZON
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+
+    //Custom Header versioning used by MICROSOFT
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    //Custom Header versioning used by MICROSOFT
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    //Media Type versioning a.k.a "content negotiation" or "accept header" used by GITHUB
+    @GetMapping(path = "/person/accept", produces = "application/vdn.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    //Media Type versioning a.k.a "content negotiation" or "accept header" used by GITHUB
+    @GetMapping(path = "/person/accept", produces = "application/vdn.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
 }
 
